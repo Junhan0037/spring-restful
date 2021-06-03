3698ed87df863d9a0d068d695b1da698274252d5
Junhan0037 <wnsgks0037@naver.com>
2021-06-03 16:38:10 +0900

    feat: HelloWorld
    - test

---
+++ src/main/java/com/springrestful/helloworld/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.springrestful.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+}
