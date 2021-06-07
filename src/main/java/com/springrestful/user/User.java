c3431a7bd053dd8ba4b7cfed6b18a46a706aa1d9
Junhan0037 <wnsgks0037@naver.com>
2021-06-07 13:49:27 +0900

    feat: Validation API
    - spring-boot-starter-validation

--- src/main/java/com/springrestful/user/User.java
+++ src/main/java/com/springrestful/user/User.java
@@ -3,6 +3,8 @@ package com.springrestful.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Data
@@ -10,7 +12,11 @@ import java.time.LocalDateTime;
 public class User {
 
     private Integer id;
+
+    @Size(min = 2, message = "Name은 2글자 이상 입력해주세요.")
     private String name;
+
+    @Past
     private LocalDateTime joinDate;
 
 }
